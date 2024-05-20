afa1b9273d0131f05b702d43b6144e610fdd004a
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-20 17:11:23 +0300

    [Common][Object Helpers] Actualization

--- src/common/helpers/object.ts
+++ src/common/helpers/object.ts
@@ -4,16 +4,13 @@ export function entries<K extends PropertyKey, V>(r: Nullable<Record<K, V>>): [K
   return Object.entries(r ?? {}).map(([k, v]) => [k as K, v as V]);
 }
 
-export function fromEntries<K extends PropertyKey, V>(v: [K, V][]): Record<K, V> {
+export function fromEntries<K extends PropertyKey, V>(v: (readonly [K, V])[]): Record<K, V> {
   return Object.fromEntries(v) as Record<K, V>;
 }
 
-export async function mapAsync<K extends PropertyKey, A, B>(
-  r: Record<K, A>,
-  f: (_: A) => Promise<B>,
-): Promise<Record<K, B>> {
-  const result: [K, B][] = await Promise.all(entries(r).map(async ([k, v]) => [k, await f(v)]));
-  return fromEntries(result);
+export function mapAsync<K extends PropertyKey, A, B>(r: Record<K, A>, f: (_: A) => Promise<B>): Promise<Record<K, B>> {
+  const parallelMapping = entries(r).map(([k, v]) => f(v).then(mapped => [k, mapped] as const));
+  return Promise.all(parallelMapping).then(fromEntries);
 }
 
 export function map<K extends PropertyKey, A, B>(r: Record<K, A>, f: (_: A) => B): Record<K, B> {
