9255f8ecf90d46f43805f5c9d13fec8ecaff52a3
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-22 10:06:47 +0300

    [143][Feature][App Structure] HTML DOM Wrapper implementation

--- src/ui/app-content/app-content.tsx
+++ src/ui/app-content/app-content.tsx
@@ -5,7 +5,9 @@ import Content from 'src/ui/app-content/content/content';
 const AppContent: React.FC = () => {
   return (
     <>
+      {/* TODO configure app bar */}
       <Content />
+      {/* TODO configure app footer container after implementation */}
     </>
   );
 };
