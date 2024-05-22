668820426147a3f7de4a94e1dd22b1b9c6a4e7cb
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-22 09:53:19 +0300

    [143][Feature][App Structure] HTML DOM Wrapper implementation

--- src/ui/app-content/app-content.tsx
+++ src/ui/app-content/app-content.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import { capitalizeLabel } from 'src/common/helpers/string';
-import { currentLang } from 'src/common/land/lang.helper';
-import AppBar from 'src/ui/app-content/app-bar/app-bar';
-import AppFooter from 'src/ui/app-content/app-footer/app-footer';
 import Content from 'src/ui/app-content/content/content';
-import MUIBox from 'src/ui/common/components/mui-box/mui-box';
-import { flexGrowNormal } from 'src/ui/common/styles/consts';
 
 const AppContent: React.FC = () => {
   return (
