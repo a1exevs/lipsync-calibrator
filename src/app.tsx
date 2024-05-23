b95a443befeec4d94f67dabfbfaf7d85f964e15a
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-23 08:26:58 +0300

    [146][Feature][App Structure] App State configuration with DOM, App Steps switch logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from 'src/store/store';
 import AppContainer from 'src/ui/app-container/app-container';
 import AppContent from 'src/ui/app-content/app-content';
 import AppWrapper from 'src/ui/app-wrapper/app-wrapper';
 
 const App: React.FC = () => {
   return (
-    // TODO Uncomment me [146]
-    // <Provider store={store}>
-    <AppWrapper>
-      <AppContainer>
-        <AppContent />
-      </AppContainer>
-    </AppWrapper>
-    // </Provider>
+    <Provider store={store}>
+      <AppWrapper>
+        <AppContainer>
+          <AppContent />
+        </AppContainer>
+      </AppWrapper>
+    </Provider>
   );
 };
 
--- src/ui/app-content/content/content.tsx
+++ src/ui/app-content/content/content.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Content: React.FC = () => {
-  // TODO Switch logic for Uploader, Animation List, 3d Model Viewer
+import { AppStep } from 'src/common/types/app';
+
+type Props = {
+  step: AppStep;
+};
+
+const Content: React.FC<Props> = ({ step }) => {
   return (
     <>
-      <div>content</div>
+      {/* TODO configure component when it'll be implemented */}
+      {step === AppStep.FILE_UPLOADER_STEP && <label>FileUploaderContainer</label>}
+      {/* TODO configure component when it'll be implemented */}
+      {step === AppStep.ANIMATION_LIST_STEP && <label>AnimationListContainer</label>}
+      {/* TODO configure component when it'll be implemented */}
+      {step === AppStep.THREE_D_MODEL_VIEWER_STEP && <label>ThreeDModelViewerContainer</label>}
     </>
   );
 };
