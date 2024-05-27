167e9cfd5a93c05cb2c68f20de159850a5fac725
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-27 09:00:29 +0300

    [152][Feature][File Uploader] File Uploader click handler for a file selection

--- src/ui/app-content/content/file-uploader/file-uploader.tsx
+++ src/ui/app-content/content/file-uploader/file-uploader.tsx
@@ -1,11 +1,15 @@
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import { Button, Typography } from '@mui/material';
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { interpolateStrings } from 'src/common/helpers/string';
 import { currentLang } from 'src/common/land/lang.helper';
+import { Nullable } from 'src/common/types/common';
 import { AnimationItem } from 'src/ui/app-content/content/animation-list/animation-list.types';
-import { supportedFormats } from 'src/ui/app-content/content/file-uploader/file-uploader.consts';
+import {
+  supportedThreeDModelFormats,
+  threeDModelFileUploaderAccept,
+} from 'src/ui/app-content/content/file-uploader/file-uploader.consts';
 import useClasses from 'src/ui/app-content/content/file-uploader/file-uploader.styles';
 import MUIBox from 'src/ui/common/components/mui-box/mui-box';
 
@@ -17,16 +21,33 @@ type Props = {
 const FileUploader: React.FC<Props> = () => {
   const classes = useClasses();
 
+  const threeDModelfileInputRef = useRef<Nullable<HTMLInputElement>>(null);
+
+  const handleThreeDModelFileChange = async (_: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
+    // TODO Implement
+  };
+
   return (
     <MUIBox className={classes.fileUploader}>
       <Typography variant="body1" color="inherit" align="center">
         {currentLang.messages.THIS_APP_FOR_CALIBRATE} <br /> {currentLang.messages.SELECT_THREE_D_FILE}
       </Typography>
-      <Button variant="contained" startIcon={<UploadFileOutlinedIcon />}>
+      <input
+        type="file"
+        accept={threeDModelFileUploaderAccept}
+        onChange={handleThreeDModelFileChange}
+        ref={threeDModelfileInputRef}
+        style={{ display: 'none' }}
+      />
+      <Button
+        variant="contained"
+        startIcon={<UploadFileOutlinedIcon />}
+        onClick={() => threeDModelfileInputRef.current?.click()}
+      >
         <label>{currentLang.labels.UPLOAD_FILE_BTN}</label>
       </Button>
       <Typography variant="body1" align="center">
-        {interpolateStrings(currentLang.messages.SUPPORTED_FORMATS, String(supportedFormats))}
+        {interpolateStrings(currentLang.messages.SUPPORTED_FORMATS, String(supportedThreeDModelFormats))}
       </Typography>
     </MUIBox>
   );
