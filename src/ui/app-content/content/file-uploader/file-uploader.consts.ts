5389341fd0417b9a0bfa3bcff6c75d203d87326d
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2024-05-27 18:05:14 +0300

    [152][Feature][File Uploader] File Uploader click handler for a file selection (#49)
    Co-authored-by: AlexEvs <aleksandrevstafiadi@gmail.com>

--- src/ui/app-content/content/file-uploader/file-uploader.consts.ts
+++ src/ui/app-content/content/file-uploader/file-uploader.consts.ts
@@ -1,3 +1,7 @@
 import { getSupportedThreeDModelExtensions } from 'src/ui/app-content/content/file-uploader/helpers/supported-three-d-model-extensions.helper';
 
-export const supportedFormats = getSupportedThreeDModelExtensions().join(', ');
+export const supportedThreeDModelFormats = getSupportedThreeDModelExtensions().join(', ');
+
+export const threeDModelFileUploaderAccept = getSupportedThreeDModelExtensions()
+  .map(extension => `.${extension}`)
+  .join(', ');
