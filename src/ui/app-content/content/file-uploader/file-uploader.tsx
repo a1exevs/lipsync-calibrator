7b73cd487ecb000ca7fc35db646a95c9bdb76537
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-28 10:56:18 +0300

    [155][Feature][File Uploader] 3d Model File data helper implementation, animation data reading

--- src/ui/app-content/content/file-uploader/file-uploader.tsx
+++ src/ui/app-content/content/file-uploader/file-uploader.tsx
@@ -1,29 +1,44 @@
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import { Button, Typography } from '@mui/material';
 import React, { useRef, useState } from 'react';
+import { Group } from 'three';
 
 import { getExtensionByFile } from 'src/common/helpers/file';
-import { isUndefined } from 'src/common/helpers/guards';
+import { isEmpty, isUndefined } from 'src/common/helpers/guards';
+import { calculatePercentage } from 'src/common/helpers/number';
 import { interpolateStrings } from 'src/common/helpers/string';
 import { currentLang } from 'src/common/land/lang.helper';
 import { Nullable } from 'src/common/types/common';
+import { AnimationItem } from 'src/ui/app-content/content/animation-list/animation-list.types';
 import { ResetErrorFn, SetErrorFn } from 'src/ui/app-content/content/error-context/error.types';
 import {
   supportedThreeDModelFormats,
   threeDModelUploaderAccept,
 } from 'src/ui/app-content/content/file-uploader/file-uploader.consts';
 import useClasses from 'src/ui/app-content/content/file-uploader/file-uploader.styles';
+import { getAvailableAnimationListByModel } from 'src/ui/app-content/content/file-uploader/helpers/animations-data-reader.helper';
+import { ThreeDModelFileLoaderFactory } from 'src/ui/app-content/content/file-uploader/helpers/file-loaders.factory';
 import { isThreeDModelExtensionSupported } from 'src/ui/app-content/content/file-uploader/helpers/supported-three-d-model-extensions.helper';
+import { SupportedThreeDModelExtension } from 'src/ui/app-content/content/three-d-model-viewer/drivers/driver-config-map.types';
 import MUIBox from 'src/ui/common/components/mui-box/mui-box';
 
 type Props = {
+  setThreeDModel: (_: Group) => void;
+  setThreeDModelExtension: (_: SupportedThreeDModelExtension) => void;
+  setAvailableAnimationList: (animations: AnimationItem[]) => void;
   setError: SetErrorFn;
   resetError: ResetErrorFn;
 };
 
-const FileUploader: React.FC<Props> = ({ setError }) => {
+const FileUploader: React.FC<Props> = ({
+  setError,
+  resetError,
+  setThreeDModel,
+  setAvailableAnimationList,
+  setThreeDModelExtension,
+}) => {
   // TODO add progress component
-  const [_, __] = useState<number>(0);
+  const [_, setProgress] = useState<number>(0);
 
   const classes = useClasses();
 
@@ -41,7 +56,19 @@ const FileUploader: React.FC<Props> = ({ setError }) => {
         throw new Error(currentLang.errors.MODEL_FILE_EXTENSION_IS_NOT_SUPPORTED);
       }
 
-      // TODO Immplement
+      const loader = ThreeDModelFileLoaderFactory.create(fileExtension);
+      const model = await loader.loadAsync(URL.createObjectURL(file), event =>
+        setProgress(calculatePercentage(event.loaded, event.total)),
+      );
+      if (isEmpty(model.animations)) {
+        throw new Error(currentLang.errors.MODEL_DOES_NOT_CONTAIN_ANIMATIONS);
+      }
+
+      setThreeDModel(model);
+      setThreeDModelExtension(fileExtension as SupportedThreeDModelExtension);
+      setAvailableAnimationList(getAvailableAnimationListByModel(model));
+
+      resetError();
     } catch (e) {
       if (e instanceof Error) {
         setError(e.message, 'error');
--- src/ui/app-content/content/file-uploader/helpers/animations-data-reader.helper.ts
+++ src/ui/app-content/content/file-uploader/helpers/animations-data-reader.helper.ts
@@ -1,4 +1,8 @@
-export function getAvailableAnimationListByModel(_: unknown): string[] {
-  // TODO [155] get names (or some IDs) by loaded 3d model
-  return [];
+import { Group } from 'three';
+
+import { AnimationItem } from 'src/ui/app-content/content/animation-list/animation-list.types';
+
+export function getAvailableAnimationListByModel(model: Group): AnimationItem[] {
+  const { animations } = model;
+  return animations.map(animation => ({ name: animation.name, uuid: animation.uuid, duration: animation.duration }));
 }
