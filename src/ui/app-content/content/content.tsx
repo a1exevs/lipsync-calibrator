a8a335e1fbeb6428b40dd55a7ec2404e849b34bd
Ilya Dragonromeo <you@example.com>
2024-05-28 13:09:56 +0300

    [158][Feature][File Uploader] Component configuration

--- src/ui/app-content/content/content.tsx
+++ src/ui/app-content/content/content.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import FileUploaderContainer from './file-uploader/file-uploader.container';
 import { AppStep } from 'src/common/types/app';
 import ErrorBar from 'src/ui/app-content/content/error-bar/error-bar';
 import ErrorProvider from 'src/ui/app-content/content/error-context/error.provider';
@@ -14,8 +15,7 @@ const Content: React.FC<Props> = ({ step }) => {
     <ErrorProvider>
       <ErrorBar />
       <ThreeDModelProvider>
-        {/* TODO configure component when it'll be implemented */}
-        {step === AppStep.FILE_UPLOADER_STEP && <label>FileUploaderContainer</label>}
+        {step === AppStep.FILE_UPLOADER_STEP && <FileUploaderContainer />}
         {/* TODO configure component when it'll be implemented */}
         {step === AppStep.ANIMATION_LIST_STEP && <label>AnimationListContainer</label>}
         {/* TODO configure component when it'll be implemented */}
