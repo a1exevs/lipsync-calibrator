bd7f50a874aad188cd9998f4ce3c99f8603c1873
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-05-28 07:54:41 +0300

    PR issue fix

--- src/ui/app-content/app-footer/app-footer.tsx
+++ src/ui/app-content/app-footer/app-footer.tsx
@@ -1,5 +1,5 @@
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import { Button, IconButton } from '@mui/material';
+import { Button } from '@mui/material';
 import React from 'react';
 
 import { currentLang } from 'src/common/land/lang.helper';
