7dab4866d4cf3ece1ced941abe5f0a6f542165b9
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2024-06-18 22:17:04 +0300

    [224][Feature][ThreeDModelViewer Nav Bar] JSON Data validator implementation (#122)
    Co-authored-by: AlexEvs <aleksandrevstafiadi@gmail.com>

---
+++ src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-validator.consts.ts
@@ -0,0 +1,12 @@
+import { Shape } from 'src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-validator.types';
+
+export const jsonStructureExample: Shape[] = [
+  {
+    shapeName: 'shapeName1',
+    data: [{ time: 0, value: 0.1 }],
+  },
+  {
+    shapeName: 'shapeName2',
+    data: [{ time: 0, value: 0.1 }],
+  },
+];
--- src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-validator.ts
+++ src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-validator.ts
@@ -1,3 +1,34 @@
-export function validateSelectedJSON() {
-  // TODO Implement
+import Ajv from 'ajv';
+
+import { isNil, isUndefined } from 'src/common/helpers/guards';
+import { interpolateStrings } from 'src/common/helpers/string';
+import { currentLang } from 'src/common/land/lang.helper';
+import { Nillable } from 'src/common/types/common';
+import { jsonShapesDataStructureScheme } from 'src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-scheme';
+import { ValidatorOutput } from 'src/ui/app-content/content/three-d-model-viewer/nav-bar/validators/json-structure-validator.types';
+
+export function validateSelectedJSONStructure(shapeDataStr: Nillable<string | ArrayBuffer>): ValidatorOutput {
+  try {
+    const validateFn = new Ajv().compile(jsonShapesDataStructureScheme);
+    const baseErrorMessage = currentLang.errors.INCORRECT_JSON_FILE_STRUCTURE;
+    if (isNil(shapeDataStr)) {
+      return { isValid: false, error: baseErrorMessage };
+    }
+    const shapeData = JSON.parse(shapeDataStr as string);
+    const valid = validateFn(shapeData);
+    if (!valid) {
+      const error = validateFn.errors?.map(e => e.message).join('; ');
+      const additionalErrorMessage = !isUndefined(error)
+        ? interpolateStrings(` ${currentLang.errors.ERRORS}`, error)
+        : '';
+
+      return { isValid: false, error: baseErrorMessage + additionalErrorMessage };
+    }
+    return { isValid: true };
+  } catch (e) {
+    if (e instanceof Error) {
+      return { isValid: false, error: e?.message };
+    }
+    return { isValid: false };
+  }
 }
