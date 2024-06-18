d16999f29fb78d3c8d2ee591ae6e0fa47a60e015
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-06-18 10:18:11 +0300

    [223][Feature][ThreeDModelViewer Nav Bar] 'Select JSON'' button click handler, json file loading logic

--- src/ui/app-content/content/three-d-model-viewer/nav-bar/nav-bar.container.tsx
+++ src/ui/app-content/content/three-d-model-viewer/nav-bar/nav-bar.container.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import { useError } from 'src/ui/app-content/content/error-context/use-error';
 import NavBar from 'src/ui/app-content/content/three-d-model-viewer/nav-bar/nav-bar';
+import { useUiBlocker } from 'src/ui/app-content/content/ui-blocker-context/use-ui-blocker';
 
 const NavBarContainer: React.FC = () => {
   // TODO Push data to store logic
   // TODO GEt access-state from store
   const allowToExportToJSON = true;
 
-  return <NavBar allowToExportToJSON={allowToExportToJSON} />;
+  const { blockUI, unblockUI } = useUiBlocker();
+  const { setError, resetError } = useError();
+
+  return (
+    <NavBar
+      allowToExportToJSON={allowToExportToJSON}
+      blockUI={blockUI}
+      unblockUI={unblockUI}
+      setError={setError}
+      resetError={resetError}
+    />
+  );
 };
 
 export default NavBarContainer;
