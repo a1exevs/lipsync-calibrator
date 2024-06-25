6420a0c41182f7c22f88916866da5588be0acbbc
AlexEvs <aleksandrevstafiadi@gmail.com>
2024-06-25 07:58:21 +0300

    [233][Feature][ThreeDModelViewer][Morph Target Plot List] Morph Target Сurve Plot List component implementation

--- src/ui/app-content/content/three-d-model-viewer/morph-target-plot-list/morph-target-plot-list.tsx
+++ src/ui/app-content/content/three-d-model-viewer/morph-target-plot-list/morph-target-plot-list.tsx
@@ -1,9 +1,36 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
-const MorphTargetPlotList: React.FC = () => {
-  // TODO Implement
+import { isNull } from 'src/common/helpers/guards';
+import { Nullable } from 'src/common/types/common';
+import { MorphTargetData } from 'src/store/slices/app/app.types';
+import MorphTargetPlot from 'src/ui/app-content/content/three-d-model-viewer/morph-target-plot-list/morph-target-plot/morph-target-plot';
+import useClasses from 'src/ui/app-content/content/three-d-model-viewer/morph-target-plot-list/morph-target-plot-list.styles';
+import MUIBox from 'src/ui/shared/components/mui-box/mui-box';
 
-  return <></>;
+type Props = {
+  morphTargetData: Nullable<MorphTargetData>;
+};
+
+const MorphTargetPlotList: React.FC<Props> = ({ morphTargetData }) => {
+  const classes = useClasses();
+
+  if (isNull(morphTargetData)) {
+    return <></>;
+  }
+
+  return (
+    <MUIBox className={classes.morphTargetPlotList}>
+      {morphTargetData.data.map((shape, index) => (
+        <MUIBox key={shape.shapeName + index}>
+          <Typography variant="h6" align="center">
+            {shape.shapeName}
+          </Typography>
+          <MorphTargetPlot points={shape.data} />
+        </MUIBox>
+      ))}
+    </MUIBox>
+  );
 };
 
 export default MorphTargetPlotList;
